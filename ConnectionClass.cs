1e9901f00c2ad4d0caad1a118739ab2ece774470
furkancinar98 <furkancinar34@hotmail.com>
2019-10-17 19:24:50 +0300

    Yazlab OOP-1

---
+++ ConnectionClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace TestSınavıOtomasyonu
+{
+    class ConnectionClass
+    {
+        public void baglanti()
+        {
+            SqlConnection baglanti = new SqlConnection(@"Data Source=PC-FURKAN\MSSQLSERVER02;Initial Catalog=TestSınavıDegerlendirme;Integrated Security=True");
+            baglanti.Open();
+        }
+       
+    }
+}
---
+++ DatabaseClass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace TestSınavıOtomasyonu
+{
+    class DatabaseClass
+    {
+        ConnectionClass conn = new ConnectionClass();
+       
+        
+        
+      //  SqlConnection connection = new SqlConnection(@"Data Source=PC-FURKAN\MSSQLSERVER02;Initial Catalog=TestSınavıDegerlendirme;Integrated Security=True");
+        SqlCommand command;
+        SqlDataReader reader;
+       
+        public void login(object ad,object parola)
+        {
+            conn.baglanti();
+            command = new SqlCommand("select * from Kullanicilar where  kullanici_adi='" + ad + "' and sifre = '" + parola + "'",conn);
+         //   connection.Open();
+            
+            reader = command.ExecuteReader();
+            if (reader.Read())        
+            MessageBox.Show("Hoşgeldiniz");
+       
+            else
+           MessageBox.Show("Hatalı Giriş","Error",MessageBoxButtons.OK);
+
+        //    connection.Close();
+
+            
+            
+        
+        }
+    }
+}
